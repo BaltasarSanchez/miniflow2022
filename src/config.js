eb6cc4834b7105b5d4c2e39bae10097978f78757
Nicolas Mastromarino <Nicolasmastrom@gmail.com>
2022-03-17 13:29:50 -0300

    config fix

--- src/config.js
+++ src/config.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({
-  path: path.resolve(process.cwd(), process.env.NODE_ENV + '.env')
+  path: path.resolve(process.cwd(), process.env.NODE_ENV || 'development' + '.env')
 });
 
-console.log(process.env.MONGO_URL)
+console.log("Mongo URL:" + process.env.MONGO_URL)
+console.log("Ruta Archivo Configuracion:" + path.resolve(process.cwd(), process.env.NODE_ENV || 'development' + '.env'))
 
 export default {
   NODE_ENV: process.env.NODE_ENV || 'development',
