dbb96b836b7376198d5b6f716f1dab60339d9300
Nicolas Mastromarino <Nicolasmastrom@gmail.com>
2022-03-18 10:03:07 -0300

    cors v4

--- src/middlewares/CorsHeaders.js
+++ src/middlewares/CorsHeaders.js
@@ -3,12 +3,24 @@ function CorsHeaders(req, res, next) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header(
         "Access-Control-Allow-Methods",
-        "POST,PUT,GET,DELETE,UPDATE,OPTIONS"
+        "POST,PUT,GET,DELETE,UPDATE"
     );
     res.header(
         "Access-Control-Allow-Headers",
-        "X-Requested-With, X-HTTP-Method-Override, Content-Type,Accept"
+        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
     );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    );
+    res.header("Access-Control-Expose-Headers", " set-cookie");
+
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 
     next();
 }
